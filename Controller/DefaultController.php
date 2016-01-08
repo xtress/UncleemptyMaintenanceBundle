028d9e7389edd0198b7cf71e84e52b708eee4dc6
empty <yarkovets.i@gmail.com>
2016-01-08 21:34:18 +0300

    initial maintenance bundle commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Uncleempty\MaintenanceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('UncleemptyMaintenanceBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ UncleemptyMaintenanceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Uncleempty\MaintenanceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UncleemptyMaintenanceBundle extends Bundle
+{
+}
